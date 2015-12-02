2106510004feaa2601052e1fc0060fada590cc2b
Petr Kocandrle <pkocandr@redhat.com>
2015-12-02 21:38:17 +0100

    Fix optional link creation

--- reporter.py
+++ reporter.py
@@ -272,7 +272,7 @@ def generate_summary(roots, boms, groupids, multiversion_gas, malformed_versions
             gav_filename = "%s$%s$%s" % (ma.groupId, ma.artifactId, ma.version)
             html += ("<li><a href=\"pages/groupid_{groupid}.html\" title=\"GroupId {groupid}\">{groupid}</a> : " + \
                      "<a href=\"pages/artifactid_{groupid}${artifactid}.html\" title=\"ArtifactId {artifactid}\">{artifactid}</a> : " + \
-                     "<a href=\"pages/artifact_version_{gav_filename}.html\">{gav}</a></li>").format(groupid=ma.groupId,
+                     "<a href=\"pages/artifact_version_{gav_filename}.html\">{version}</a></li>").format(groupid=ma.groupId,
                                                                                                      artifactid=ma.artifactId,
                                                                                                      version=ma.version,
                                                                                                      gav_filename=gav_filename)
